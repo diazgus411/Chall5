38e6369ee7acda04070afec4fbc3c8b685639a7d
Gustavo <diazgus411@gmail.com>
2023-04-23 20:20:44 -0700

    Completed Commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# Chall5
+# <Challenge 5: Creating a Work Planner>
+
+## Description
+
+<li>Used my knew knowledge of JQuery and previous knowledge of JavaScript to create a work planner
+<li>Learned how to work better with JQuery
+<li>Learned more about BootStrap and the quality of life change it provides for CSS
+<li>Learned more about JavaScript being mixed inbetween JQuery code and having it still work
+<li>Learned more about HTML and BootStrap being used together within the HTML
+
+## Installation
+
+<p>Installing this code is very easy. Following the link to the repo listed down below, we can see the code that was used to get this planner working properly. Having your work team use this is very simple as well, all you have to do it send them the link to the planner itself, and information can be shared easily within  the planner.
+
+## Usage
+
+<p>Using the actual planner itself is very simple, you simply click on a date, put whatever information you want in it, and click the save button. Boom! Just like that your planner has whatever you wrote in it!
+
+# Screenshot
+![alt text](./Assets/Chall5.png)
+---
+# <Link to Repo With Source Code>
+<p>https://github.com/diazgus411/Chall5
+---
--- script.js
+++ script.js
@@ -1,16 +1,66 @@
 $(document).ready(function() {
     // Get the current date using moment.js
     var currentDate = moment().format("dddd, MMMM Do YYYY");
-    
+  
     // Display the current date in the header
-    $("#currentDay").text(currentDate);
-    
+    $("#currentDay").text(currentDate)
+  
     // Update the time every second
     setInterval(function() {
       // Get the current time using moment.js
-      var currentTime = moment().format("h:mm:ss A");
-      
+      var currentTime = moment().format("h:mm:ss A")
+  
       // Display the current time in the header
-      $("#currentDay").text(currentDate + " - " + currentTime);
+      $("#currentDay").text(currentDate + " - " + currentTime)
     }, 1000);
+  
+    // Add click event listener to all save buttons
+    $('.saveBtn').on('click', function() {
+      // Get the text entered in the corresponding textarea
+      var text = $(this).siblings(".description").val().trim()
+      // Get the hour for the corresponding time block
+      var hour = $(this).siblings(".hour").text().trim()
+  
+      // Save the data to localStorage using the hour as the key
+      localStorage.setItem(hour, text);
+    });
+  
+    // Load saved data from localStorage on page load
+    $(".hour").each(function() {
+      var hour = $(this).text().trim();
+      var text = localStorage.getItem(hour);
+  
+      if (text !== null) {
+        $(this).siblings(".description").val(text);
+      }
+    });
+  
+    // Display current date and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  
+    // Get the current hour in 24-hour format
+    var currentHour = moment().hour();
+  
+    // Loop through each timeblock
+    $(".time-block").each(function() {
+      var blockHour = parseInt($(this).attr("data-hour"));
+  
+      // Add classes based on the current hour and timeblock hour
+      if (blockHour < currentHour) {
+        $(this).addClass("past");
+      } else if (blockHour === currentHour) {
+        $(this).addClass("present");
+      } else {
+        $(this).addClass("future");
+      }
+    });
   });
+
+
+
+
+
+
+
+
+
